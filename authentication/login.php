a01d28fdff23abd85f52585e97b5e67721162029
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-10-12 14:47:18 +0300

    5th commit

--- authentication/login.php
+++ authentication/login.php
@@ -1,15 +1,20 @@
 <?php
+include "../connect.php";
 
-include "../connect.php" ; 
+// Get user input
+$usersemail = filterRequest("email");
+$userspassword = sha1($_POST['password']);
 
+// Prepare query
+$stmt = $con->prepare("SELECT * FROM users WHERE users_email = ? AND users_password = ? AND TRIM(users_approved) = 1");
+$stmt->execute([$usersemail, $userspassword]);
 
- //Get user input (signup data)
-$usersemail = filterRequest("email") ; 
-// $usersphone = filterRequest("phone") ; 
-$userspassword  = sha1($_POST['password'])  ;
+$count = $stmt->rowCount();
 
-
-getData("users", "users_email = ? AND users_password = ?", array($usersemail, $userspassword));
-
-
+if ($count > 0) {
+    $data = $stmt->fetch(PDO::FETCH_ASSOC); // fetch user data
+    echo json_encode(array("status" => "success", "data" => $data));
+} else {
+    echo json_encode(array("status" => "failure"));
+}
 ?>
