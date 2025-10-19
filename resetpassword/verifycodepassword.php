dbcd8526c2145a204ccf9d5d05f9cb2138cda184
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-10-19 12:29:23 +0300

    5th commit

---
+++ resetpassword/verifycodepassword.php
@@ -0,0 +1,21 @@
+<?php
+include "../connect.php";
+$email = trim($_POST['email']);
+$code  = trim($_POST['users_verificationcode']);
+
+$stmt = $con->prepare("SELECT * FROM users WHERE TRIM(users_email) = ? AND TRIM(users_verificationcode) = ?");
+$stmt->execute([$email, $code]);
+$count = $stmt->rowCount();
+
+if ($count > 0) {
+     echo json_encode(["status" => "success", ]);
+  
+} else {
+    echo json_encode(["status" => "failure", "message" => "Invalid verification code or email"]);
+}
+
+
+
+
+
+?>
